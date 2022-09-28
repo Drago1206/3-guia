1a9ee871b4fbe9058051c211d601aa16898d4812
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2022-09-28 12:08:22 -0500

    1

---
+++ main.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded",()=>{
+    let gall=1
+    while(gall>0){
+        let alt=0,nh=0,cal=0,pes=0
+        pes=Number(prompt("Ingrese el peso de la gallina: "))
+        alt=Number(prompt("ingrese la altura de la gallina:  "))
+        nh=Number(prompt("Ingrese el numero de huevos que pone:  "))
+        cal=(pes*alt)/nh
+        
+        document.write(`____________________________________________`)
+        document.write(`El precio de las ventas por kilo es de: `)
+        break
+    }
+})
